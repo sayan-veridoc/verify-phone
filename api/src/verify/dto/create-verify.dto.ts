183a4d4d61fc55098648dafa2ae8b39f6885a797
Sayan <dassayan004@gmail.com>
2024-07-05 11:16:18 +0530

    fix

--- api/src/verify/dto/create-verify.dto.ts
+++ api/src/verify/dto/create-verify.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsNotEmpty } from 'class-validator';
+import { IsString, IsNotEmpty, Matches } from 'class-validator';
 
 export class VerifyPhoneDto {
   @ApiProperty()
   @IsNotEmpty({ message: 'Phone No is required' })
   @IsString({ message: 'Phone No must be a string' })
+  @Matches(/^\+?[1-9]\d{1,14}$/, {
+    message: 'Phone No must be a valid phone number',
+  })
   phone: string;
 }
